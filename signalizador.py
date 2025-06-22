09f11848c9dbec7bede6db19b217819f83990ea5
Claudio577 <chideki.yoshida@gmail.com>
2025-06-22 10:30:44 -0300

    Update signalizador.py

--- signalizador.py
+++ signalizador.py
@@ -17,3 +17,24 @@ def buscar_noticias(api_key, limite=20):
         print("Erro ao buscar notícias:", resposta.status_code)
         return []
 
+from transformers import pipeline, BertTokenizer, BertForSequenceClassification
+
+# Carregar modelo BERT
+tokenizer = BertTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+model = BertForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+sentiment_pipeline = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+
+def analisar_sentimentos(noticias):
+    sentimentos = []
+    for noticia in noticias:
+        resultado = sentiment_pipeline(noticia)[0]
+        score = resultado['score']
+        label = resultado['label']
+        # Converter estrelas para sentimento numérico
+        if label in ['1 star', '2 stars']:
+            sentimentos.append(-score)
+        elif label in ['4 stars', '5 stars']:
+            sentimentos.append(score)
+        else:
+            sentimentos.append(0)
+    return sentimentos
